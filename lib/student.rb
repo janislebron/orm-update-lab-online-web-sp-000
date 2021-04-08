d549fdde73b82dd81bbc865931eb3ec1342fb3ac
Janis Lebron <janislebron@gmail.com>
2021-04-08 22:24:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,8 +63,7 @@ attr_accessor :id, :name, :grade
  end
 
  def update
-   sql = "UPDATE students
-   SET name = ?, grade = ? WHERE id = ?"
+   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
    DB[:conn].execute(sql, self.name, self.grade, self.id)
  end
 end
